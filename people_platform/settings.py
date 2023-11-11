40879864d43a14cb7c19e344ebaafd0c50daf6df
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2023-11-11 20:00:42 -0300

    Setting postgresql

--- people_platform/settings.py
+++ people_platform/settings.py
@@ -91,9 +91,13 @@ WSGI_APPLICATION = 'people_platform.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "app"),
+        "USER": os.getenv("POSTGRES_USER", "app"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "app"),
+        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+        "PORT": os.getenv("POSTGRES_PORT", "15432"),
     }
 }
 
