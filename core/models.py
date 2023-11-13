36fb3b97df3ceeed3312397a53a30dd4f8d95f14
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2023-11-13 20:54:07 -0300

    Add models for Company and CostCenter

--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from .common.managers import BaseModel, BaseModelManager
 
-# Create your models here.
+class Company(BaseModel):
+    name = models.TextField()
+    cnpj = models.CharField(max_length=14, unique=True, null=True)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=2)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class CostCenter(BaseModel):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="cost_centers")
+    code = models.CharField(max_length=10)
+    description = models.CharField(max_length=100)
+    cost = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    created_at = models.DateField(auto_now_add=True)
