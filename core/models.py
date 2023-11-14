f98fec0224879625d7552f2787e55560982a1f17
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2023-11-13 21:11:04 -0300

    Add model for Employee

--- core/models.py
+++ core/models.py
@@ -20,6 +20,9 @@ class CostCenter(BaseModel):
     cost = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
     created_at = models.DateField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.code}-{self.description}-{self.company.name}"
+
 
 class Department(BaseModel):
     company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="departments")
@@ -27,3 +30,17 @@ class Department(BaseModel):
     name = models.CharField(max_length=100)
     integration_code = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
+
+class Employee(BaseModel):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="employees")
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name="employees")
+    full_name = models.CharField(max_length=125)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=25)
+    birth_date = models.DateField()
+    entry_date = models.DateField()
+    departure_date = models.DateField(null=True, blank=True)
+    city = models.CharField(max_length=100)
