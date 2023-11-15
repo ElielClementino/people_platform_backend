7e4bf5f65279172ddef1cc90a1e3b203ce16d838
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2023-11-14 23:32:11 -0300

    Improve admin panel

--- core/admin.py
+++ core/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from core.models import Company, CostCenter, Department, Employee
 
 
-admin.site.register([Company, CostCenter, Department, Employee])
+admin.site.register([CostCenter, Department, Employee])
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'cnpj')
+    search_fields = ('name', 'cnpj')
+    list_filter = ('name',)
+    ordering = ('name',)
